5b0e0d15ea3e63cd9fcc94b9732e0a7c98d8625a
Simonas Baltulionis <sbaltulionis@kayak.com>
2023-05-17 10:00:27 +0300

    #3

---
+++ AsyncWorkshop/AsyncAwaitUnderTheHood/ExecutionContextDemo.cs
@@ -0,0 +1,41 @@
+﻿using System.Dynamic;
+
+namespace AsyncAwaitUnderTheHood;
+
+public class ExecutionContextDemo
+{
+    private static readonly AsyncLocal<string> UserId = new();
+    //private static string UserId = "";
+    //[ThreadStatic] private static string UserId = "none";
+    private readonly ServiceA _serviceA = new();
+
+    public static string GetUserId() => UserId.Value;
+    public static string SetUserId(string userId) => UserId.Value = userId;
+    
+    public async Task ExecuteAsync(string userId)
+    {
+        SetUserId(userId);
+        await _serviceA.ExecuteOperationAsync();
+    }
+}
+
+public class ServiceA
+{
+    private readonly ServiceB _serviceB = new ServiceB();
+
+    public async Task ExecuteOperationAsync()
+    {
+        await Task.Delay(1000);
+        await _serviceB.ExecuteOperationAsync();
+    }
+}
+
+
+public class ServiceB
+{
+    public async Task ExecuteOperationAsync()
+    {
+        await Task.Delay(1000);
+        Console.WriteLine(ExecutionContextDemo.GetUserId());
+    }
+}
---
+++ AsyncWorkshop/AsyncAwaitUnderTheHood/TimerAwaitable.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AsyncAwaitUnderTheHood;
+
+public class TimerAwaitable
+{
+    private readonly double _interval;
+
+    public TimerAwaitable(double interval)
+    {
+        _interval = interval;
+    }
+
+    public TimerAwaiter GetAwaiter() => new TimerAwaiter(_interval);
+
+    public class TimerAwaiter : INotifyCompletion
+    {
+        private readonly System.Timers.Timer _timer;
+        private Action _continuation;
+
+        public TimerAwaiter(double interval)
+        {
+            _timer = new System.Timers.Timer(interval);
+            _timer.Elapsed += (sender, e) => { IsCompleted = true; _continuation?.Invoke(); };
+            _timer.AutoReset = false;
+        }
+
+        public bool IsCompleted { get; private set; }
+
+        public void OnCompleted(Action continuation)
+        {
+            _continuation = continuation;
+            _timer.Start();
+        }
+
+        public void GetResult() { }
+    }
+}
